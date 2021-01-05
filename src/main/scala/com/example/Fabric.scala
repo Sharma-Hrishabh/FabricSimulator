6a2299127a3006b90c4823458a7543cd382d7d63
Sharma-Hrishabh <30hrs1999@gmail.com>
2021-01-05 12:46:00 +0530

    change endorser spawn loc

--- src/main/scala/com/example/Fabric.scala
+++ src/main/scala/com/example/Fabric.scala
@@ -1,20 +1,19 @@
 //#full-example
 package com.example
 
-
 import akka.actor.typed.ActorRef
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
-import com.example.InitMain.TxnProposal
+import com.example.InitMain.bootStrap
+import com.example.Endorser.submitTransaction
 
 
 object Endorser {
-
   // final case class Endorsed(whom:String, replyTo: ActorRef[])
   final case class submitTransaction(whom:String)
   def apply(): Behavior[submitTransaction] = Behaviors.receive { (context, message) =>
-    context.log.info("Endorser: Hello {}!", message.whom)
+    context.log.info("Endorsed!", message.whom)
     // message.replyTo ! Endorsed(message.whom, context.self)
     Behaviors.same
   }
@@ -23,43 +22,36 @@ object Endorser {
 
 object Client {
 
- final case class createTransaction(whom:String)
+ final case class createTransaction(whom:String,replyTo: ActorRef[submitTransaction])
  def apply(): Behavior[createTransaction] =  Behaviors.receive { (context, message) =>
-    context.log.info("Client: Hello {}!", message.whom)
-    val replyTo = context.spawn(Endorser(), "endorser")
-    replyTo ! Endorser.submitTransaction(message.whom)
+    context.log.info("Client: Submitting {}!", message.whom)
+    message.replyTo ! Endorser.submitTransaction(message.whom)
     Behaviors.same
   }
 }
 
 object InitMain {
 
-final case class TxnProposal(name: String)
+final case class bootStrap(name: String)
 
-  def apply(): Behavior[TxnProposal] =
+  def apply(): Behavior[bootStrap] =
     Behaviors.setup { context =>
 
       val client = context.spawn(Client(),"client")
       //#create-actors
 
       Behaviors.receiveMessage { message =>
-        // val replyTo = context.spawn(Endorser(), "endorser")
-        client ! Client.createTransaction(message.name)
-        // greeter ! Greeter.Greet(message.name, replyTo)
+        val replyTo = context.spawn(Endorser(), "endorser")        
+        client ! Client.createTransaction(message.name,replyTo)
+
         Behaviors.same
       }
     }
 }
 
-
 //#main-class
 object FabricSimulator extends App {
   //#actor-system
-  val initMain: ActorSystem[InitMain.TxnProposal] = ActorSystem(InitMain(), "FabricSimulator")
-  //#actor-system
-
-  //#main-send-messages
-  initMain ! TxnProposal("TX:A")
-  //#main-send-messages
+  val initMain: ActorSystem[InitMain.bootStrap] = ActorSystem(InitMain(), "FabricSimulator")
+  initMain ! bootStrap("TX:A")
 }
-//#main-class
